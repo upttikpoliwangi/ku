c00fe262fd1bd107688f84c7854854aed959ad83
ajil23 <azrilpraya@gmail.com>
2025-05-26 20:18:52 +0800

    user

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -44,10 +44,37 @@ class CreateAdminUserSeeder extends Seeder
 		
 		$permissions = Permission::where('name','adminlte.darkmode.toggle')->orWhere('name','logout.perform')->orWhere('name','home.index')->orWhere('name','login.show')->pluck('id','id')->all();
 		
-		$role = Role::create(['name' => 'terdaftar']);
+				$role = Role::create(['name' => 'terdaftar']);
 		$role->syncPermissions($permissions);
 		$role = Role::create(['name' => 'operator']);
 		$role->syncPermissions($permissions);
-		
+		$role = Role::create(['name' => 'mahasiswa']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'dosen']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'pegawai']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'direktur']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'wadir1']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'wadir2']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'wadir3']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'kaprodi']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'kajur']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'p2m']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'kaunit']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'kalab']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'keuangan']);
+		$role->syncPermissions($permissions);
+		$role = Role::create(['name' => 'sekjur']);
+		$role->syncPermissions($permissions);
     }
 }
